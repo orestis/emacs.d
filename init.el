a760d7a86c65ab31fc0a52985427cacd6e954288
Orestis Markou <orestis@orestis.gr>
2018-10-05 13:04:30 +0300

    initial commit

---
+++ init.el
@@ -0,0 +1,49 @@
+;; Hello, world
+
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://stable.melpa.org/packages/") t)
+
+;; keep the installed packages in .emacs.d
+(setq package-user-dir (expand-file-name "elpa" user-emacs-directory))
+(package-initialize)
+
+;; update the package metadata is the local cache is missing
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(setq user-full-name "Orestis Markou"
+      user-mail-address "orestis@orestis.gr")
+
+;; Always load newest byte code
+(setq load-prefer-newer t)
+
+;; reduce the frequency of garbage collection by making it happen on
+;; each 50MB of allocated data (the default is on every 0.76MB)
+(setq gc-cons-threshold 50000000)
+
+;; warn when opening files bigger than 100MB
+(setq large-file-warning-threshold 100000000)
+
+(defconst bozhidar-savefile-dir (expand-file-name "savefile" user-emacs-directory))
+
+;; create the savefile dir if it doesn't exist
+(unless (file-exists-p bozhidar-savefile-dir)
+  (make-directory bozhidar-savefile-dir))
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
+
+;; MY STUFF TO BE REPLACED
+(setq icomplete-mode t)
+
+(unless (package-installed-p 'clojure-mode)
+  (package-install 'clojure-mode))
+
+(unless (package-installed-p 'cider)
+  (package-install 'cider))
